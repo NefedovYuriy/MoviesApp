232bb8e9f51dc546c74854c2547cee71c5f1e334
NefedovYuriy <95857760+NefedovYuriy@users.noreply.github.com>
2023-12-10 17:21:49 +0300

    V2 Class

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,84 +1,138 @@
-import React, { useEffect, useState } from 'react';
-import { Row, Col, Spin, Alert } from 'antd';
+import { React, Component } from 'react';
 
-import { Main } from '../Main';
 import './app.css';
+import { MovieList } from '../MovieList';
+import { ErrorIndicator } from '../Error';
+import { RatedList } from '../RatedList';
+import { Context } from '../../context';
+import { ToggleTab } from '../ToggleTab';
+import { GuestSession, ServiceApi } from '../../api';
 
-export function App() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [isOnline, setIsOnline] = useState(true);
-  // const apiKey = '905e917ccfddc7effe602b7d5165147a';
-  // const urlArrMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
-  // const url = 'https://api.themoviedb.org/3/movie/popular?api_key=905e917ccfddc7effe602b7d5165147a';
-  useEffect(() => {
-    const handleOnlineStatus = () => setIsOnline(navigator.onLine);
-
-    window.addEventListener('online', handleOnlineStatus);
-    window.addEventListener('offline', handleOnlineStatus);
-
-    return () => {
-      window.removeEventListener('online', handleOnlineStatus);
-      window.removeEventListener('offline', handleOnlineStatus);
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pageTab: 'search',
+      genres: [],
+      guestToken: '',
+      dataRated: {
+        moviesRated: [],
+        totalPage: 0,
+        page: 1,
+      },
+      error: false,
+      errorMassage: '',
+      screenWidth: window.innerWidth,
     };
-  }, []);
+    this.api = new ServiceApi();
+    this.guest = new GuestSession();
+  }
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=905e917ccfddc7effe602b7d5165147a')
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data.results);
-        setIsLoading(false);
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+  handleResize = () => {
+    this.setState({ screenWidth: window.innerWidth });
+  };
+
+  onError = (e) => {
+    console.log('Error!');
+    console.log(e);
+    console.log(e.message);
+    this.setState({
+      error: true,
+      errorMessage: e.message,
+    });
+  };
+
+  changePage = (tab) => {
+    this.setState({
+      pageTab: tab,
+    });
+  };
+
+  getGenres = () => {
+    this.api
+      .getGenres()
+      .then((res) => {
+        this.setState({ genres: res.genres });
       })
-      .catch(() => {
-        setIsLoading(false);
-        setError('An error occurred, please try again later');
-      });
-  }, []);
+      .catch(this.onError);
+  };
 
-  if (!isOnline) {
+  getToken = () => {
+    const token = localStorage.getItem('guest');
+    if (token) this.setState({ guestToken: token });
+    else {
+      this.guest
+        .getToken()
+        .then((token) => {
+          this.setState({ guestToken: token });
+          localStorage.setItem('guest', `${token}`);
+        })
+        .catch(this.onError);
+    }
+  };
+
+  getAllMovies = (movieName) => {
+    return this.api.getAllMovies(`${movieName}`);
+  };
+
+  getPageMovies = (movieName, numPage) => {
+    return this.api.getPageMovies(`${movieName}`, `${numPage}`);
+  };
+
+  sendRateStars = (id, countStars) => {
+    this.guest.postRateStars(this.state.guestToken, id, countStars).catch(this.onError);
+  };
+
+  getGuestSession = (page = 1) => {
+    return this.guest.getSession(this.state.guestToken, page);
+  };
+
+  getPageSession = (page) => {
+    return this.api.getSession(this.state.guestToken, page);
+  };
+
+  componentDidMount() {
+    this.getGenres();
+    this.getToken();
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  render() {
+    const { genres, pageTab, error, errorMessage, screenWidth } = this.state;
+    const errorMessage1 = error ? <ErrorIndicator errorMessage={errorMessage} /> : null; // ?????Error
+    const viewTab = (pageTab) => {
+      if (pageTab === 'search') {
+        return (
+          <MovieList
+            pageTab={pageTab}
+            onError={this.onError}
+            sendRateStars={this.sendRateStars}
+            getAllMovies={this.getAllMovies}
+            screenWidth={screenWidth}
+          />
+        );
+      } else if (pageTab === 'rated') {
+        return (
+          <RatedList
+            pageTab={pageTab}
+            onError={this.onError}
+            getGuestSession={this.getGuestSession}
+            screenWidth={screenWidth}
+          />
+        );
+      }
+    };
     return (
-      <div className="container">
-        <div className="center">
-          <Alert message="no network" type="error" />
-        </div>
-      </div>
+      <Context.Provider value={genres}>
+        <section className="container">
+          <ToggleTab changePage={this.changePage} active={pageTab} />
+          {viewTab(pageTab)}
+          {errorMessage1}
+        </section>
+      </Context.Provider>
     );
   }
-  return (
-    <div className="container">
-      {isLoading && (
-        <div className="center">
-          <Spin size="large" />
-        </div>
-      )}
-      {!!error && (
-        <div className="center">
-          <Alert message={error} type="error" />
-        </div>
-      )}
-      {!isLoading && !error && data?.length && (
-        <Row gutter={[16, 16]}>
-          {data.map((elem) => (
-            <Col xs={24} md={12} key={elem.id}>
-              <div className="movie">
-                <img
-                  className="movie-img"
-                  src={`https://image.tmdb.org/t/p/original/${elem.poster_path}`}
-                  alt={elem.title}
-                />
-                <div className="movie-text">
-                  <h3 className="movie-title">{elem.title}</h3>
-                  <span className="movie-description">{elem.overview}</span>
-                </div>
-              </div>
-            </Col>
-          ))}
-        </Row>
-      )}
-      <Main />
-    </div>
-  );
 }
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,21 +1,86 @@
-import React from 'react';
-import { Button, Card, Flex, Typography } from 'antd';
-import './movieCard.css';
+import { React, Component } from 'react';
+import { format } from 'date-fns';
 
-export function MovieCard() {
-  return (
-    <Card className="cardStyle" bodyStyle={{ padding: 0, overflow: 'hidden' }}>
-      <Flex justify="space-between">
-        <img alt="avatar" src="" className="imgStyle" />
-        <Flex vertical align="flex-end" justify="space-between" style={{ padding: 32 }}>
-          <Typography.Title level={3}>
-            “antd is an enterprise-class UI design language and React UI library.”
-          </Typography.Title>
-          <Button type="primary" href="https://ant.design" target="_blank">
-            Get Start
-          </Button>
-        </Flex>
-      </Flex>
-    </Card>
-  );
+import './movieCard.css';
+import { Genres } from '../Genres/Genres';
+import { RateStars } from '../RateStars';
+
+export class MovieCard extends Component {
+  render() {
+    const { id, title, dateRelease, description, poster, dataGenres, rating, countStars, sendRateStars, screenWidth } =
+      this.props;
+    const posterIMG = (poster) => {
+      const defaultImage =
+        'https://publicimagedata.s3.amazonaws.com/stable_diffusion_thumbnail/1/hyper_realistic_oil_painting_movie_poster_of_a_fat_toad_wearing_anaglyph_red_-_blue_glasses_with_popcorn_pot_vibrant_colors_high_contrast_-H_640_-C_10.0_-n_9_-i_-S_2866264059_ts-1660401670_idx-4_thum';
+      const posterImage = `https://image.tmdb.org/t/p/w500${poster}`;
+      return poster === null ? defaultImage : posterImage;
+    };
+
+    const formatText = (description) => {
+      let string = description;
+      if (description.length > 200) {
+        string = string.slice(0, string.indexOf(' ', 100));
+        string += ' ...';
+      }
+      return string;
+    };
+
+    const formatData = (dateRelease) => {
+      if (!dateRelease) return null;
+      return format(new Date(dateRelease), 'MMMM d, yyyy');
+    };
+
+    const getColor = (rating) => {
+      let colorRating = 'card__description-rate ';
+      if (rating >= 7) return (colorRating += 'super');
+      if (rating >= 5 && rating < 7) return (colorRating += 'high');
+      if (rating >= 3 && rating < 5) return (colorRating += 'medium');
+      if (rating >= 0 && rating < 3) return (colorRating += 'low');
+    };
+
+    const getStars = (countStars) => {
+      if (countStars === undefined) return 0;
+      else return countStars;
+    };
+    if (screenWidth > 910) {
+      return (
+        <li className="movie__card">
+          <div>
+            <img className="card__image" src={posterIMG(poster)}></img>
+          </div>
+          <div className="card__description">
+            <div className="card__description-header">
+              <h5 className="card__description-title">{title}</h5>
+              <p className={getColor(rating)}>{rating.toFixed(1)}</p>
+            </div>
+            <p className="movie__description-release">{formatData(dateRelease)}</p>
+            <div className="movie__description-genre">
+              <Genres dataGenres={dataGenres} />
+            </div>
+            <p className="movie__description-paragraph">{formatText(description)}</p>
+            <RateStars sendRateStars={sendRateStars} stars={getStars(countStars)} id={id} />
+          </div>
+        </li>
+      );
+    }
+    return (
+      <li className="movie__card">
+        <div className="card__info">
+          <img className="card__image" src={posterIMG(poster)}></img>
+          <div className="card__description-header">
+            <div>
+              <h5 className="card__description-title">{title}</h5>
+              <p className="movie__description-release">{formatData(dateRelease)}</p>
+              <div className="movie__description-genre">
+                <Genres dataGenres={dataGenres} />
+              </div>
+            </div>
+            <p className={getColor(rating)}>{rating.toFixed(1)}</p>
+          </div>
+        </div>
+        <p className="movie__description-paragraph">{formatText(description)}</p>
+        <RateStars sendRateStars={sendRateStars} stars={getStars(countStars)} id={id} />
+      </li>
+    );
+  }
 }
